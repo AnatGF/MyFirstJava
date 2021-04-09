a9450f21320bdfc4d2917e8dea5f786550805481
Anat <anatgif@gmail.com>
2021-04-09 20:50:04 +0300

    Adding height

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Human maor = new Human(28, 'M', 1.70f);
+        Human maor = new Human(28, 'F', 1.70f);
         maor.setAge(99);
-        maor.setGender('F');
+        maor.setGender('M');
         maor.introduceYourself();
         System.out.println(maor.getAge());
         System.out.println(maor.getGender());
