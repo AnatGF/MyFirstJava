deaafd59fce220e7aa88788c85c3246b3162e514
Anat <anatgif@gmail.com>
2021-04-09 21:19:27 +0300

    Changing the height

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Human maor = new Human(28, 'F', 1.70f);
+        Human maor = new Human(28, 'F', 1.80f);
         maor.setAge(99);
         maor.setGender('M');
         maor.introduceYourself();
